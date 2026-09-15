152bd61f8c6059b2b5c485917d349c6aa7caa750
Guddu Kumar <150721059+guddu13kumar@users.noreply.github.com>
2026-09-16 01:50:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,12 @@
-const CACHE_NAME = "sre-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "https://cdn.tailwindcss.com",
-  "https://unpkg.com/react@18/umd/react.production.min.js",
-  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"
-];
+const CACHE_NAME = "sre-pos-v2";
+const urlsToCache = ["./", "./index.html"];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match("./index.html")))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
